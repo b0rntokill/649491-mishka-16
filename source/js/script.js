6d4a0e116c21e77a206656f0f07ede314c2d9639
b0rntokill <zappbranigan39@gmail.com>
2019-05-12 21:26:31 +0200

    Внес указанные исправления, добавил все недостающие элементы

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,4 @@
 var menu = document.querySelector(".page-header__nav");
-
 menu.classList.remove("page-header__nav--nojs");
 
 var menuToggle = menu.querySelector(".page-header__toggle");
@@ -18,3 +17,36 @@ menuToggle.addEventListener("click", function (evt) {
 
 var mapInteractive = document.querySelector(".contacts__map");
 mapInteractive.classList.remove("contacts__map--nojs");
+
+var modal = document.querySelector(".modal");
+var modalOpen = document.querySelector(".special-offers__button");
+var overlay = document.querySelector(".overlay");
+
+modalOpen.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  modal.classList.add("modal--show");
+  overlay.classList.add("overlay--show");
+});
+
+overlay.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.remove("modal--show");
+    overlay.classList.remove("overlay--show");
+});
+
+overlay.addEventListener("touchend", function (evt) {
+    evt.preventDefault();
+    modal.classList.remove("modal--show");
+    overlay.classList.remove("overlay--show");
+});
+
+window.addEventListener("keydown", function (evt) {
+
+  if (evt.keyCode === 27) {
+    if (modal.classList.contains("modal--show")) {
+      evt.preventDefault();
+      modal.classList.remove("modal--show");
+      overlay.classList.remove("overlay--show");
+    }
+  }
+});
